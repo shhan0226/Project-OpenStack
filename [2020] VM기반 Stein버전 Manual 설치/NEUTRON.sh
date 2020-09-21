e89bca21d0550b6cbce2f2d78a685af2f7409b34
shhan0226 <shhan0226@gmail.com>
2020-09-21 19:57:15 +0000

    no up

---
+++ [2020] VM기반 Stein버전 Manual 설치/NEUTRON.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+read -p "What is openstack passwrd? : " STACK_PASSWD
+echo "$STACK_PASSWD"
+
+##########################################
+echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
+echo "NOVA Reg. Mariadb ..."
+
