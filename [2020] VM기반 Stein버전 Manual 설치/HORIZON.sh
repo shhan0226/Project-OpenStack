c94a9cf9c59697a7f8c01d47f54dcaa1e1b6c829
shhan0226 <shhan0226@gmail.com>
2020-09-26 10:27:04 +0000

    NETRON update

--- [2020] VM기반 Stein버전 Manual 설치/HORIZON.sh
+++ [2020] VM기반 Stein버전 Manual 설치/HORIZON.sh
@@ -1,9 +1,19 @@
-#! /bin/bash
+#!/bin/bash
 
 read -p "What is openstack passwrd? : " STACK_PASSWD
 echo "$STACK_PASSWD"
 
+ifconfig
+read -p "Input IP: " SET_IP
+echo "$SET_IP"
+sync
+
 ##########################################
 echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
-echo "NOVA Reg. Mariadb ..."
+echo "Service Install ..."
+apt install openstack-dashboard -y
+
+crudini 
+
+
 
--- [2020] VM기반 Stein버전 Manual 설치/KEYSTONE.sh
+++ [2020] VM기반 Stein버전 Manual 설치/KEYSTONE.sh
@@ -1,4 +1,4 @@
-#! /bin/bash
+#!/bin/bash
 
 read -p "What is openstack passwrd? : " STACK_PASSWD
 echo "$STACK_PASSWD"
