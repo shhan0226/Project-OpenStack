6e760e45e2180a17f778914df0367b7f93cc1bb5
shhan0226 <57717799+shhan0226@users.noreply.github.com>
2020-10-16 15:51:51 +0900

    Update GLANCE-ARM.sh

--- [2020] ARM-Server기반 Stein버전 Manual 설치/GLANCE-ARM.sh
+++ [2020] ARM-Server기반 Stein버전 Manual 설치/GLANCE-ARM.sh
@@ -70,9 +70,10 @@ service glance-api restart
 echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
 echo "download img"
 . admin-openrc
-wget http://download.cirros-cloud.net/0.4.0/cirros-0.4.0-x86_64-disk.img
 
-openstack image create "cirros" --file cirros-0.4.0-x86_64-disk.img --disk-format qcow2 --container-format bare --public
+wget http://download.cirros-cloud.net/0.5.1/cirros-0.5.1-arm-disk.img
+
+openstack image create "cirros" --file cirros-0.5.1-arm-disk.img --disk-format qcow2 --container-format bare --public
 
 openstack image list
 
